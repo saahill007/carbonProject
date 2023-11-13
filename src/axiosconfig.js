098b396e40d6c13c431522256c5251a845b90d33
kurra-Ruchitha <kurraruchitha06@gmail.com>
2023-11-12 23:20:56 -0500

    emailnotificationchanges

--- src/axiosconfig.js
+++ src/axiosconfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://3.140.241.230:3000";
+const baseURL = "http://18.191.232.91:3000";
 // const baseURL = "http://localhost:3000";
 
 const axiosInstance = axios.create({
